7c3c7b6cacebcf2c32dabef54828dcb049b97d15
Danny Guinther <dannyguinther@gmail.com>
2014-07-10 08:57:21 -0400

    Delegate BuildBlocks.build directly to default_builder#build

--- lib/building_blocks.rb
+++ lib/building_blocks.rb
@@ -1,9 +1,16 @@
+require 'forwardable'
 require 'building_blocks/version'
 require 'building_blocks/builders/builder_builder'
 
 # Primary BuildingBlocks object providing an interface to build/define other
 # builders using builders that build new builder objects.
 module BuildingBlocks
+
+  class << self
+    extend Forwardable
+    def_delegator :default_builder, :build
+  end
+
   # The default builder used for defining new builders when {build build}
   # is invoked.
   # @see build
@@ -39,21 +46,14 @@ module BuildingBlocks
     @default_builder = builder
   end
 
-  # Defines/builds a new builder object using the **builder** object
-  # provided or the {default_builder default builder}. Any
-  # additional **args** provided will be passed unmodified to the `build` method
-  # of the **builder**. If a **block** is given it will also be passed to
-  # the `builder` unmodified.
-  # @param [#build] builder Custom builder to use for
-  #   for this definition. If no value is supplied the
-  #   {DEFAULT_DEFINITION_BUILDER default builder} is used.
-  # @return [Object] The result of invoking `build` on the
-  #   `builder` (often an instance of the `builder`).
-  # @see default_builder
-  def self.build(builder = self.default_builder, *args)
-    block_given? ? builder.build(*args, &Proc.new) : builder.build(*args)
-  end
-
   # Namespace for builder classes
   module Builders; end
+
+  # @!method self.build(*args, &block)
+  #   Builds a new object using the the {default_builder default builder}. Any
+  #   **args** or **block** provided are delegated directly to the `build` method
+  #   of the {default_builder default builder}.
+  #   @return [Object] The result of invoking `build` on the
+  #     {#default_builder default builder}.
+  #   @see default_builder
 end
